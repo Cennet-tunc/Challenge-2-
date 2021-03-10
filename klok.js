6f8580cc0350f7658a1aa25791a4bbf4b790d52f
Cennet-tunc <cennet-tunc@hotmail.com>
2021-03-11 00:31:21 +0100

    Werking digitale klok (aangepast aan huidige tijdstip)

--- klok.js
+++ klok.js
@@ -1,1 +1,19 @@
-js
+ setInterval(()=>{
+        const time = document.getElementById("time");
+        var date = new Date();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+       
+        if(seconds < 10){
+          seconds = "0" + seconds;
+        }
+        if(minutes < 10){
+          minutes = "0" + minutes;
+        }
+        if(hours < 10){
+          hours = "0" + hours;
+        }
+        document.getElementById("time").innerHTML = hours + ":" + minutes + ":" + seconds;
+      });
+
