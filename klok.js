e5da3e9345adb3459d7768a69181236af129d2fe
Cennet-tunc <cennet-tunc@hotmail.com>
2021-03-12 00:31:58 +0100

    Achtergrond per 6 uur tijd

--- klok.js
+++ klok.js
@@ -17,3 +17,32 @@
         document.getElementById("time").innerHTML = hours + ":" + minutes + ":" + seconds;
       });
 
+
+function dayandnight(){
+  var current = new Date();
+  var day_night = current.getHours();
+    if (day_night >= 6 && day_night<12){
+      // Ochtend
+      var bodyColor = document.getElementsByTagName("BODY")[0];
+      bodyColor.style.backgroundImage = 'url("Afbeeldingen/ochtend.jpg")';
+    }
+    else if (day_night >= 12 && day_night<18){
+      //Middag
+      var bodyColor = document.getElementsByTagName("BODY")[0];
+       bodyColor.style.backgroundImage = 'url("Afbeeldingen/middag.jpg")';
+    }
+     else if (day_night >= 18 && day_night<24){
+      //Avond
+      var bodyColor = document.getElementsByTagName("BODY")[0];
+       bodyColor.style.backgroundImage = 'url("Afbeeldingen/avond.jpg")';
+    }
+    else if (day_night >= 0 && day_night<6){
+      //Nacht
+      var bodyColor = document.getElementsByTagName("BODY")[0];
+       bodyColor.style.backgroundImage = 'url("Afbeeldingen/nacht.jpg")';
+    }
+   }
+
+   dayandnight();
+
+   
